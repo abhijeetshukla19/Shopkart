42b9825fe7b1fc0f66bf8d7d7d623f4be276697a
Abhijeet Shukla <abhijeet.shukla194@gmail.com>
2025-07-26 19:49:16 +0530

    Update settings.py

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'monish.khan@itvedant.com'
+EMAIL_HOST_USER = 'abhijeet.shukla194@gmail.com'
 EMAIL_HOST_PASSWORD = 'lpcl pbwv ohnm svyi'
